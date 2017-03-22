4d1f40eaa569c12279965f8838c02e31e1254029
jorgegomezv <jorgegverdejo@gmail.com>
2017-03-22 00:49:28 -0300

    Servidor - Base de datos corriendo correctamente.

---
+++ app.js
@@ -0,0 +1,20 @@
+'use strict'
+
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+
+//Cargar rutas
+
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.json());
+
+//Configurar cabeceras http
+
+//rutas base
+
+app.get('/pruebas', function(req,res){
+	res.status(200).send({message: 'Bienvenido a Musify'})
+})
+
+module.exports = app;
