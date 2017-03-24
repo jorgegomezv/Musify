c93e085dd9eb83fde045b8075b5f9a2dd9fefc18
jorgegomezv <jorgegverdejo@gmail.com>
2017-03-24 11:04:48 -0300

    Agregar Comentarios index.js

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 'use strict'
 
-var mongoose = require('mongoose');
-var app = require('./app');
-var port = process.env.PORT || 3977;
+var mongoose = require('mongoose');//Usar funciones de MongoDB
+var app = require('./app');//Necesita del servidor WEB.
+var port = process.env.PORT || 3977;//Puerto LocalHost
 
 //Coneccion a la base de datos.
-mongoose.connect('mongodb://localhost:27017/musify', (err , res) =>{
+mongoose.connect('mongodb://localhost:27017/musify', function(err , res){
 	if(err){
 		throw err;
 	}else{
 		console.log("La conexion a la base de datos esta corriendo correctamente.");
+		//Servidor funcionando
 		app.listen(port , function(){
 			console.log("Servidor del APIRest de musica esuchando en HTTP://localhost:" + port);
 		})
